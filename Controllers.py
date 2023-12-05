0fdb6ed82e7583ee79daf6fcb38ca423ca8f0718
JuanPabloHincapieF <jphf0527@gmail.com>
2023-12-04 20:43:54 -0500

    Bug fixies

--- Controllers.py
+++ Controllers.py
@@ -1,60 +1,60 @@
 import csv
 
-def crear_registro(archivo, nuevo_registro):
-    with open(archivo, mode='a', newline='') as file:
-        writer = csv.writer(file)
-        writer.writerow(nuevo_registro)
+def create_record(file, new_record):
+    with open(file, mode='a', newline='') as f:
+        writer = csv.writer(f)
+        writer.writerow(new_record)
 
-def leer_registros(archivo):
-    with open(archivo, mode='r') as file:
-        reader = csv.reader(file)
+def read_record(file):
+    with open(file, mode='r') as f:
+        reader = csv.reader(f)
         for row in reader:
             print(row)
 
-def actualizar_registro(archivo, id_actualizar, nuevo_registro):
-    with open(archivo, mode='r') as file:
-        rows = list(csv.reader(file))
+def update_record(file, updating_id, new_record):
+    with open(file, mode='r') as f:
+        rows = list(csv.reader(f))
         for i, row in enumerate(rows):
-            if row[0] == id_actualizar:
-                rows[i] = nuevo_registro
+            if row[0] == updating_id:
+                rows[i] = new_record
 
-    with open(archivo, mode='w', newline='') as file:
-        writer = csv.writer(file)
+    with open(file, mode='w', newline='') as f:
+        writer = csv.writer(f)
         writer.writerows(rows)
 
-def borrar_registro(archivo, id_borrar):
-    with open(archivo, mode='r') as file:
-        rows = list(csv.reader(file))
-        new_rows = [row for row in rows if row[0] != id_borrar]
+def delete_record(file, deleting_id):
+    with open(file, mode='r') as f:
+        rows = list(csv.reader(f))
+        new_rows = [row for row in rows if row[0] != deleting_id]
 
-    with open(archivo, mode='w', newline='') as file:
-        writer = csv.writer(file)
+    with open(file, mode='w', newline='') as f:
+        writer = csv.writer(f)
         writer.writerows(new_rows)
 
-# Ejemplo de uso:
-archivo_csv = 'contraseñas.csv'
+# Example:
+file_csv = 'passwords.csv'
 
-# Crear registro
-nuevo_registro = ['1', 'Correo', 'usuario1', 'contraseña123']
-crear_registro(archivo_csv, nuevo_registro)
+# create record
+new_record = ['1', 'Mail', 'user', 'password123']
+create_record(file_csv, new_record)
 
-# Leer registros
-print("Registros actuales:")
-leer_registros(archivo_csv)
+# Read records
+print("Actual records:")
+read_record(file_csv)
 
-# Actualizar registro
-id_actualizar = '1'
-nuevo_registro_actualizado = ['1', 'Correo', 'usuario_actualizado', 'nueva_contraseña']
-actualizar_registro(archivo_csv, id_actualizar, nuevo_registro_actualizado)
+# Update record
+updating_id = '1'
+new_record_updated = ['1', 'Mail', 'modified_user', 'modified_password']
+update_record(file_csv, updating_id, new_record_updated)
 
-# Leer registros después de la actualización
-print("\nRegistros después de la actualización:")
-leer_registros(archivo_csv)
+# Read records after updating
+print("\nRecords after updating:")
+read_record(file_csv)
 
-# Borrar registro
-id_borrar = '1'
-borrar_registro(archivo_csv, id_borrar)
+# delete record
+deleting_id = '1'
+delete_record(file_csv, deleting_id)
 
-# Leer registros después de borrar
-print("\nRegistros después de borrar:")
-leer_registros(archivo_csv)
+# Read records after deleting:
+print("\nRecords after deleting:")
+read_record(file_csv)
